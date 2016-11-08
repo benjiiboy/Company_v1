f80d1683607ead843634dc3d1fe0f5c5d9200dda
benjamin lindgaard <benjaminlindgaard@ka-net.dk>
2016-11-08 13:07:41 +0100

    Lavede en constructor

--- Sandbox/InsertCodeHere.cs
+++ Sandbox/InsertCodeHere.cs
@@ -11,7 +11,7 @@ namespace Sandbox
 
 
             // Add you test of the Employee class and derived classes here
-
+            
 
 
             // The LAST line of code should be ABOVE this line
---
+++ Sandbox/Worker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sandbox
+{
+    class Worker : Employee
+    {
+       private string skill;
+
+            public Worker(String name, int salaryPerMonth)
+            : base(name, salaryPerMonth)
+        {
+            
+        }
+        
+    }
+}
